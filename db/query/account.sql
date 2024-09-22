9770b358e975ea8407095aa72dc42e2c91608de1
TomoyaOgawa <5stream418@gmail.com>
2024-09-23 01:09:09 +0900

    feat: add update and delete query

--- db/query/account.sql
+++ db/query/account.sql
@@ -19,4 +19,14 @@ ORDER BY id
 LIMIT $1
 OFFSET $2;
 
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
 
