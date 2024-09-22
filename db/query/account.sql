b33637a3d555bacae56a0ffab72d5e91885ce858
TomoyaOgawa <5stream418@gmail.com>
2024-09-23 00:59:21 +0900

    feat: update query and generate code

--- db/query/account.sql
+++ db/query/account.sql
@@ -8,3 +8,15 @@ INSERT INTO accounts (
     $2,
     $3
 ) RETURNING id, owner, balance, currency, created_at;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+
